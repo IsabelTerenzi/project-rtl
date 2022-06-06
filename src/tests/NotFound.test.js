3e548bcc4b4980dd7fb7f7be00969c78bb1d27de
Isabel Terenzi <bel.terenzi@gmail.com>
2022-06-06 18:04:12 -0300

    requisito 4 - not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testa a página Not Found', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it('Testa se a página contém um h2 com o texto correto', () => {
+    const notFound = screen
+      .getByRole('heading', { name: /page requested not found/i, level: 2 });
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem correta', () => {
+    const image = screen.getByAltText(/Pikachu crying because the page/i);
+
+    expect(image).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
